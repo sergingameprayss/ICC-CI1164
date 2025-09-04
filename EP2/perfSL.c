3215a78d31aa752ea8f66e936717c91febd80de0
SERGIO SIVONEI DE SANT ANA FILHO <sssaf24@inf.ufpr.br>
2025-09-04 13:51:47 -0300

    comecei a arrumar as coisas do likwid

--- EP2/perfSL.c
+++ EP2/perfSL.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <time.h>
+#include <likwid.h>
 
 #include "eliminacaoGauss.h"
 #include "gaussSeidel.h"
@@ -18,18 +19,27 @@ int main () {
 
     SistLinear_t *sl2 = dupSisLin(sl);
 
+    LIKWID_MARKER_INIT;
+    LIKWID_MARKER_START("EG_TEST");
+    rtime_t time_EG = timestamp();
+
     triangulariza(sl);
     retrosubst(sl, Xeg);
     residuo(sl, Xeg, Reg, sl->n);
 
-    int it = gaussSeidel(sl2, Xgs, TOL, MAXIT, &norma);
-    residuo(sl2, Xgs, Rgs, sl2->n);
-
     printf("EG:\n");
-    printf("COLOCAR TEMPO DPS   ms\n");
+    printf("%.8e\n", timestamp() - time_EG);
     prnVetor(Xeg, sl->n);
     prnVetor(Reg, sl->n);
     printf("\n");
+
+    LIKWID_MARKER_STOP("EG_TEST");
+    LIKWID_MARKER_CLOSE;
+
+    int it = gaussSeidel(sl2, Xgs, TOL, MAXIT, &norma);
+    residuo(sl2, Xgs, Rgs, sl2->n);
+
+    
 
     printf("GS [ %d iterações ]:\n", it);
     printf("COLOCAR TEMPO DPS   ms\n");
